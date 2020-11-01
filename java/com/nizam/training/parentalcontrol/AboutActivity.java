f242c5127203405315336fbe285890efe562cde9
mydeen.k <mydeen.k@zohocorp.com>
2020-11-01 17:58:46 +0530

    init comm

--- java/com/nizam/training/parentalcontrol/AboutActivity.java
+++ java/com/nizam/training/parentalcontrol/AboutActivity.java
@@ -1,7 +1,8 @@
 package com.nizam.training.parentalcontrol;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
